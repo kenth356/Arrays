9bf63c3ad42ad6158bc6c970c2a2d557e62bbb3c
kenth356 <kjsamonte27@gmail.com>
2025-05-25 10:35:29 +0800

    second commit

---
+++ putvalue2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int numbers[5];
+    cout << "\n[ENTER FIVE NUMBERS TO BE PUT IN THE ARRAY]\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "\nNUMBER " << i + 1 << ": ";
+        cin >> numbers[i];
+    }
+
+    cout << "\n[THE UPDATED VALUE OF ARRAYS OF NUMBERS ARE]\n";
+    cout << right << setw(14) << "{" << numbers[0] << ", " << numbers[1] << ", " << numbers[2] << ", " << numbers[3] << ", "
+    << numbers[4] << "}" << endl;
+}
