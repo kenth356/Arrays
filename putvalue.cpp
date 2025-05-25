35d5e4d3fbe1ba29ff052c59c05e06744569b2a5
kenth356 <kjsamonte27@gmail.com>
2025-05-25 10:16:40 +0800

    adding

---
+++ putvalue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[5];
+    numbers[0] = {50};
+    numbers[1] = {51};
+    numbers[2] = {52};
+    numbers[3] = {53};
+    numbers[4] = {54};
+
+    cout << "\nThe array of numbers are now = {" << numbers[0] << ", " << numbers[1] << ", " << numbers[2] << ", "
+    << numbers[3] << ", " << numbers[4] << "}";
+}
